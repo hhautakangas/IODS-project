a9be82e2fb3bd7b56fe05e50f66fd52c4afc0d7f
hhautakangas <hhautaka@lm4-fim07-fh05.local>
2017-02-02 08:27:53 +0200

    move file to another file
    studio ex to data file

--- data/week2rstudio.R
+++ data/week2rstudio.R
@@ -9,6 +9,8 @@ dim(LEARN)
 str(LEARN)
 summary(LEARN)
 
+sum(LEARN$gender == "F")
+sum(LEARN$gender == "M")
 
 library(ggplot2)
 p1 <- ggplot(LEARN, aes(x = Attitude, y = Points, col = gender))
@@ -33,9 +35,14 @@ library(GGally)
 # create a more advanced plot matrix with ggpairs()
 p <- ggpairs(LEARN, mapping = aes(col = gender, alpha = 0.3), lower = list(combo = wrap("facethist", bins = 20)))
 
-# draw the plot
+# draw the plot and add title
 p
+LEARNp <- p + ggtitle("Learning 2014 data summary")
+p2
+
+
 p3 <- p + geom_smooth(method = "lm")
+p3
 qplot(Attitude, Points, data = LEARN) + geom_smooth(method = "lm")
 
 p4 <- p3 + ggtitle("Student's attitude versus exam points")
@@ -43,15 +50,15 @@ p4
 
 ## linear regression
 # dependent variable Points
-# explanatory variables Age, gender and Deep
+# explanatory variables Age, gender and Attitude
 
 lm1 <- lm(Points ~ Age+ gender+ Deep, data=LEARN)
 lm1
 summary(lm1)
 
-lm2 <- lm(Points ~ Attitude+ gender+ Deep, data=LEARN)
-lm2
-summary(lm2)
+lm1 <- lm(Points ~ Attitude+ gender+ Deep, data=LEARN)
+lm1
+summary(lm1)
 
 lm3 <- lm(Points ~ Attitude+ Age + Deep, data=LEARN)
 lm3
@@ -61,11 +68,16 @@ lm4 <- lm(Points ~ Attitude+ Stra + Deep, data=LEARN)
 lm4
 summary(lm4)
 
-lm5 <- lm(Points ~ Attitude, data=LEARN)
-lm5
-summary(lm5)
+lm2 <- lm(Points ~ Attitude, data=LEARN)
+lm2
+summary(lm2)
+
+
+plot(Attitude, Points, xlab="Attitude", ylab="Points", main="Attitude vs. Points")
+abline(lm2)
 
 ## for validation
 par(mfrow= c(2,2))
-plot(lm5, which=c(1:2,5))
+plot(lm2, which=c(1:2,5))
 
+install.packages("knitr")
