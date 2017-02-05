43e62416e963d362f9314d023286a68ba2a8a56c
hhautakangas <hhautaka@dsl-hkibrasgw3-58c024-169.dhcp.inet.fi>
2017-02-05 15:14:08 +0200

    week 3, 1
    data wrangling

---
+++ alc_data.R
@@ -0,0 +1,5 @@
+## Heidi Hautakangas
+## 5.2.2017
+## Introduction to Open Data Science Week 3: Data wrangling 
+## This week we are learning to combine different datasets
+## Data source: https://archive.ics.uci.edu/ml/machine-learning-databases/00356/
