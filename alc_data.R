f442cd474237ba67851b9892c1301a54ebd755b7
hhautakangas <hhautaka@dsl-hkibrasgw3-58c3d8-220.dhcp.inet.fi>
2017-02-17 18:03:29 +0200

    week 4
    homework

--- alc_data.R
+++
@@ -1,5 +0,0 @@
-## Heidi Hautakangas
-## 5.2.2017
-## Introduction to Open Data Science Week 3: Data wrangling 
-## This week we are learning to combine different datasets
-## Data source: https://archive.ics.uci.edu/ml/machine-learning-databases/00356/
---
+++ data/create_human.R
@@ -0,0 +1,44 @@
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+## structure
+str(hd)
+dim(hd)
+str(gii)
+dim(gii)
+summary(hd)
+summary(gii)
+
+names(hd)
+
+names(hd)[1]<-"HDIrank"
+names(hd)[3]<-"HDI"
+names(hd)[4]<-"life-expectancy"
+names(hd)[5]<-"education_exp"
+names(hd)[6]<-"education_mean"
+names(hd)[7]<-"GNIperCapita"
+names(hd)[8]<-"GNIindex"
+
+names(gii)[1]<-"GIIrank"
+names(gii)[3]<-"GII"
+names(gii)[4]<-"MaternalMort"
+names(gii)[5]<-"BirthRate"
+names(gii)[6]<-"ParliamentRep"
+names(gii)[7]<-"SecEduF"
+names(gii)[8]<-"SecEduM"
+names(gii)[9]<-"LabourPartF"
+names(gii)[10]<-"LabourPartM"
+
+#he first one should be the ratio of Female and Male populations with secondary education in each country.
+#(i.e. edu2F / edu2M). 
+#The second new variable should be the ratio of labour force participation of females and males in each country 
+#(i.e. labF / labM). 
+gii <- mutate(gii, edu2_ratio = (SecEduF/SecEduM))
+gii <- mutate(gii, labourP_ratio = (LabourPartF/LabourPartM))
+
+library(dplyr)
+
+BOTH <- inner_join(hd, gii, by = "Country", suffix=c(".hd",".gii"))
+head(BOTH)
+write.table(BOTH,"data/human.txt", quote=FALSE, row.names=FALSE)
+                                                                                                                                                                                                                                                                    
