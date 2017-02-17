433d15858b9de670963e47ba813e85410347fb95
hhautakangas <hhautaka@dsl-hkibrasgw3-58c3d8-220.dhcp.inet.fi>
2017-02-17 18:09:20 +0200

    week 4

--- data/create_human.R
+++ data/create_human.R
@@ -1,7 +1,8 @@
+# read both datas to R
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
 
-## structure
+## structure, dimension and summary
 str(hd)
 dim(hd)
 str(gii)
@@ -9,6 +10,7 @@ dim(gii)
 summary(hd)
 summary(gii)
 
+#check names and rename variables to both data
 names(hd)
 
 names(hd)[1]<-"HDIrank"
@@ -33,11 +35,15 @@ names(gii)[10]<-"LabourPartM"
 #(i.e. edu2F / edu2M). 
 #The second new variable should be the ratio of labour force participation of females and males in each country 
 #(i.e. labF / labM). 
+# Mutate new variables 
+# the ratio of Female and Male populations with secondary education
 gii <- mutate(gii, edu2_ratio = (SecEduF/SecEduM))
+#the ratio of labour force participation of females and males
 gii <- mutate(gii, labourP_ratio = (LabourPartF/LabourPartM))
 
 library(dplyr)
 
+# join the data set and save it to data folder
 BOTH <- inner_join(hd, gii, by = "Country", suffix=c(".hd",".gii"))
 head(BOTH)
 write.table(BOTH,"data/human.txt", quote=FALSE, row.names=FALSE)
