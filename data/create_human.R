49be2966227a2e742631ada75c64663e5d929e77
hhautakangas <hhautaka@lm6-fimm5-usbe.pc.helsinki.fi>
2017-02-23 11:22:38 +0200

    week 5

--- data/create_human.R
+++ data/create_human.R
@@ -4,7 +4,12 @@
 # http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv
 # http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv
 
-
+library(dplyr)
+library(ggplot2)
+library(MASS)
+library(GGally)
+library(tidyr)
+library(corrplot)
 
 
 
@@ -54,7 +59,7 @@ library(dplyr)
 # join the data set and save it to data folder
 BOTH <- inner_join(hd, gii, by = "Country", suffix=c(".hd",".gii"))
 head(BOTH)
-write.table(BOTH,"data/human.txt", quote=FALSE, row.names=FALSE)
+write.table(BOTH,"data/human.txt", row.names=FALSE)
 
 
 
@@ -64,13 +69,15 @@ write.table(BOTH,"data/human.txt", quote=FALSE, row.names=FALSE)
 ## DATA WRANGLING week 5
 
 #rename data
-HUM <- BOTH
+HUM <- read.table("data/human.txt", as.is=T, header=T)
 head(HUM)
 str(HUM)
 library(stringr)
 
 ## mutate GNI to numeric
-HUM$GNI <- as.numeric(HUM$GNIindex)
+HUM$GNI<- HUM$GNIperCapita
+str_replace(HUM$GNI, pattern=",", replace ="") %>% as.numeric
+
 
 # I rename again the variables to match names in exercise instructions
 names <- c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
@@ -85,7 +92,7 @@ names(HUM)[13]<-"Parli.F"
 names(HUM)
 
 ## create a subset that contains only variables: "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F"
-HUM2<- select(HUM, one_of(names))
+HUM2<- HUM[names]
 head(HUM2)
 names(HUM2)
 ## remove NA??s
@@ -96,17 +103,16 @@ str(HUM2b)
 tail(HUM2b)
 
 # next there were asked to remove all observations which relate to regions instead of countries
-# but there is no such observations in my data, since they were filtered all ready in the previous steps because of
-# containing missing values (7 last observations in the HUM data)
+HUM2c<-HUM2b[1:((nrow(HUM2b)-7)),]
 
 # Next set country as a row name and then remove the country variable
-rownames(HUM2b) <- HUM2b$Country
-HUM2b<- HUM2b[,2:9]
-dim(HUM2b)
+rownames(HUM2c) <- HUM2c$Country
+HUM2c<- HUM2c[,2:9]
+dim(HUM2c)
 # data has now 155 observations and 8 variables instead of 9 as the exercise info says,
 # because the country column was removed (and the original subset was supposed to contain only those 9 columns listed)
 # (and also already mutated data provided for the analysis contains 8 variables instead of 9)
 
 # save data
-write.table(HUM2b, "data/human.txt", quote=F, row.names = T, col.names = T)
+write.table(HUM2b, "data/human.txt", row.names = T, col.names = T)
 
